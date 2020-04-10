f29027f03fd137615aca20035c54cf6eee3461bf
hiraras <kazilagui@163.com>
2020-04-10 18:19:06 +0800

    注释

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const serverHandle = (req, res) => {
   const { url, method } = req;
   // 允许跨域
   res.setHeader('Access-Control-Allow-Origin', '*');
+  // 浏览器发送options请求时，可能会报 Request header field content-type is not allowed by Access-Control-Allow-Headers in preflight response. 这个错误
+  // 可以通过添加下面代码解决
   res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers');
   req.path = url.split('?')[0];
   req.query = querystring.parse(url.split('?')[1]);
@@ -59,9 +61,8 @@ function getResponse(req, res) {
 
   // 未命中路由
   res.writeHead(404, { 'Content-type': 'text/plain' });
-  res.write('404 Not Found\n');
+  res.write('<h1>404 not found</h1>');
   res.end();
 }
-
 
 module.exports = serverHandle;
