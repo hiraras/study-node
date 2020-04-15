6c1c2e22b354e59e066da1d56ba899fe7bb7803b
hiraras <kazilagui@163.com>
2020-04-15 18:28:15 +0800

    reids demo

---
+++ operate_redis/index.js
@@ -0,0 +1,19 @@
+
+const redis = require('redis');
+
+const port = 6379;
+const host = '127.0.0.1';
+const redisClient = redis.createClient(port, host);
+redisClient.on('error', err => {
+  console.log.err(err);
+});
+
+redisClient.set('realname', 'zhangsan2', redis.print);
+redisClient.get('realname', (err, val) => {
+  if (err) {
+    console.err(err);
+    return;
+  }
+  console.log(val);
+  redisClient.quit();
+});
