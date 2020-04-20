7522ce2fde321e79b50ba481374902a229181ff7
hiraras <kazilagui@163.com>
2020-04-20 09:31:44 +0800

    登出

--- src/router/user.js
+++ src/router/user.js
@@ -4,23 +4,28 @@ const { login } = require('../controller/user');
 const { responseWrapper } = require('../../common/utils');
 const { set } = require('../db/redis');
 
-const getCookieExpires = () => {
+const getCookieExpires = (time) => {
   const d = new Date();
-  d.setTime(d.getTime() + 86400000);
+  d.setTime(d.getTime() + time);
   return d.toGMTString();
 }
+
 
 const handleUserRouter = (req, res) => {
   const { method, path, body, session } = req;
   // 登录
   if (method === CONSTANT.METHODS.POST && path === '/api/user/login') {
     return login(body).then(data => {
-      res.setHeader('Set-Cookie', `userId=${session.userId}; path=/; httpOnly; expires=${getCookieExpires()}`);
+      res.setHeader('Set-Cookie', `userId=${session.userId}; path=/; httpOnly; expires=${getCookieExpires(86400000)}`);
       set(req.session.userId, data[0]);
       return responseWrapper(Promise.resolve(data));
     }).catch(err => {
       return responseWrapper(Promise.reject(err));
     });
+  }
+  if (method === CONSTANT.METHODS.GET && path === '/api/user/logout') {
+    res.setHeader('Set-Cookie', `userId=${session.userId}; path=/; httpOnly; expires=${getCookieExpires(-1)}`);
+    return responseWrapper(Promise.resolve(true));
   }
 }
 
